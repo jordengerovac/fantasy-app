7fd5588c1a1be25636d741bef1780e585b91a241
Mitchell Emery <red_emery@hotmail.com>
2018-02-10 14:56:56 -0500

    Inital commit with pod

--- Fantasy-App/ViewController.swift
+++ Fantasy-App/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Fuzi
 
 class ViewController: UIViewController {
 
---
+++ Pods/Target Support Files/Pods-Fantasy-App/Pods-Fantasy-App-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Fantasy_App : NSObject
+@end
+@implementation PodsDummy_Pods_Fantasy_App
+@end
